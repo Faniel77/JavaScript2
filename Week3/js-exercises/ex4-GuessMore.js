2dc0e1b941a1345c904de7b8148a5c29baf7d942
Fanuel Asfaha <yemanefanuel15@gmail.com>
2020-08-22 08:45:37 +0200

    Guess more 4

--- Week3/js-exercises/ex4-GuessMore.js
+++ Week3/js-exercises/ex4-GuessMore.js
@@ -16,9 +16,10 @@ function f1(val) {
 }
 f1(x);
 console.log(x);
+//The Output is 9. variable 'x' has a primitive value and primitives are copied by their value. primitives are independent entities, so in this case the variable inside the function is independent of the variable outside the function. and the increment by 1 applies only to the variable inside the function and when we console.log(x) its result is still 9.
 
 const y = {
-  x: 9
+  x: 9,
 };
 
 function f2(val) {
@@ -27,3 +28,4 @@ function f2(val) {
 }
 f2(y);
 console.log(y);
+//The value of x is 10. object variables are copied by their reference. when we use object values for  variables, its address/reference in the memory is stored in the variable not the value. so when we change the value of the object, its changes are visible in the variables with the same reference to the object
