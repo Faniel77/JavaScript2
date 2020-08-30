467f007e9131cdc19e595cd13c08de3dbb0b53ae
Fanuel Asfaha <yemanefanuel15@gmail.com>
2020-08-30 11:48:51 +0200

    updated ex-about me

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -9,23 +9,38 @@
  5. See HTML
  6. Create a new img element and set its src attribute to a picture of you.Append that element to the page.
  */
+
 'use strict';
+
 // add a font family to the body
 document.body.style.fontFamily = 'arial, sans-serif';
 
 // select the spans by their ID's and add text to them
+const ulElement = document.getElementById('ulElement');
+ulElement.style.background = 'red';
+ulElement.style.borderRadius = '5px';
+ulElement.style.width = 'calc(100% - 400px)';
+ulElement.style.margin = '0 auto';
 const nickName = document.getElementById('nickname');
 nickName.textContent = 'Don';
 const favFood = document.getElementById('fav-food');
 favFood.textContent = 'Fish';
 const homeTown = document.getElementById('hometown');
 homeTown.textContent = 'Asmara, Eritrea';
-const liElement = document.querySelectorAll('li');
+const liElements = document.querySelectorAll('li');
+// liElements.style.listStyleType = 'none';
 
-//loop through the li's and add them a class name
-for (let i = 0; i < liElement.length; i++) {
-  liElement[i].className = 'list-item';
+// loop through the li's and add them a class name
+for (let i = 0; i < liElements.length; i++) {
+  liElements[i].className = 'list-item';
+  liElements[i].style.listStyleType = 'none';
+  liElements[i].style.fontSize = '1.5rem';
+  liElements[i].style.padding = '0.8rem';
 }
 const myPhoto = document.createElement('img');
-myPhoto.src = 'photo.jpg'; //add photo source
-document.body.appendChild(myPhoto); //append photo to the page
+myPhoto.src = 'photo.jpg'; // add photo source
+myPhoto.style.display = 'block';
+myPhoto.style.borderRadius = '50%';
+myPhoto.style.width = '18.75rem';
+myPhoto.style.margin = '0 auto';
+document.body.appendChild(myPhoto); // append photo to the page
